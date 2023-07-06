7fbda713f489d0cf75877ee25003b8628b046e5f
gabrielribery <opportunity.ruoss@gmail.com>
2023-07-06 09:11:23 +0200

    2 seiten erstellt 3 spalten bootstrap und header

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP Form Validation</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css">
+</head>
+<body>
+  <header style="background-color: blue; color: white;">
+    <h1>header</h1>
+  </header>
+
+  <div class="container mt-4">
+    <div class="row">
+      <div class="col-md-4">
+        <h2>Spalte 1</h2>
+        <p>Platzhalter denke ich</p>
+      </div>
+      <div class="col-md-4">
+        <h2>Spalte 2</h2>
+        <p>Hier will ich die Form</p>
+      </div>
+      <div class="col-md-4">
+        <h2>Spalte 3</h2>
+        <p>Platzhalter 2 oder so</p>
+      </div>
+    </div>
+  </div>
+
+  <footer style="background-color: blue; color: white;" class="mt-4">
+    <h3>footer</h3>
+  </footer>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
