5242574c5dfb7a7d3327c9f3c99e5258be86a7fd
gabrielribery <opportunity.ruoss@gmail.com>
2023-07-06 16:50:53 +0200

    finaltouch für heute

--- janeinquiz/janein.php
+++ janeinquiz/janein.php
@@ -1,4 +1,4 @@
-<?php
+<?php //nein 1 ja 0...
 
 function ynquestion($qid, $questionTxt, $answers, $correctAnswerIndex) {
     echo "<p>&nbsp;</p>
@@ -8,7 +8,7 @@ function ynquestion($qid, $questionTxt, $answers, $correctAnswerIndex) {
     <input type='radio' id='r-$qid-1' name='radio-$qid' value='$answers[1]'>
     <label for='r-$qid-1'>$answers[1]</label><br><br>";
 
-    echo "<input type='hidden' name='correctAnswerIndex' value='$correctAnswerIndex'>"
+    echo "<input type='hidden' name='correctAnswerIndex' value='$correctAnswerIndex'>";
 
 }
 
