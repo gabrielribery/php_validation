b82c1a830e58321c03583703b6f682bdd0c68fab
gabrielribery <opportunity.ruoss@gmail.com>
2023-07-06 16:40:00 +0200

    function erstellt

--- janeinquiz/janein.php
+++ janeinquiz/janein.php
@@ -1,15 +1,17 @@
 <?php
-$qid = "01";  //frage id 
-$questionTxt = "Bist du dumm?";
-$answers = array("ja", "nein");
-$correctAnswerIndex = 0;
-// $points = 1;
 
-echo "<p>&nbsp;</p>
+function ynquestion() {
+    echo "<p>&nbsp;</p>
     <p>$questionTxt</p>
     <input type='radio' id='r-$qid-0' name='radio-$qid' value='$answers[0]'>
     <label for='r-$qid-0'>$answers[0]</label><br>
     <input type='radio' id='r-$qid-1' name='radio-$qid' value='$answers[1]'>
     <label for='r-$qid-1'>$answers[1]</label><br><br>";
 
+}
+$qid = "01";  //frage id 
+$questionTxt = "Bist du dumm?";
+$answers = array("ja", "nein");
+$correctAnswerIndex = 0;
+// $points = 1;
 ?>
