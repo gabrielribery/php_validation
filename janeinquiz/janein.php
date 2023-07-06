494c848f42c0c4c6b726bc1057414c1d733c102f
gabrielribery <opportunity.ruoss@gmail.com>
2023-07-06 16:08:28 +0200

    labels beschriftet

--- janeinquiz/janein.php
+++ janeinquiz/janein.php
@@ -6,8 +6,8 @@ $answers = array("ja", "nein");
 echo "<p>&nbsp;</p>
     <p>$questionTxt</p>
     <input type='radio' id='ja' name='radio-01' value='$answers[0]'>
-    <label for='ja'>Ja</label><br>
+    <label for='ja'>$answers[0]</label><br>
     <input type='radio' id='nein' name='radio-01' value='$answers[1]'>
-    <label for='nein'>Nein</label><br><br>";
+    <label for='nein'>$answers[1]</label><br><br>";
 
 ?>
