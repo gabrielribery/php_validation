70f4734589d43465fb6b88a313ebfd6cd6b4f097
gabrielribery <opportunity.ruoss@gmail.com>
2023-07-06 15:39:22 +0200

    finish

--- result.php
+++ result.php
@@ -27,6 +27,7 @@
         /*
           P03: Gib die ge-posteten Key-Value-Paare als Liste aus.
         */
+        prettyPrint($_POST);
  
       ?>
 
@@ -35,7 +36,47 @@
       Geht es dir heute gut: <?php echo $_POST["radio-mood"]; ?>.<br>
 
       Du hast folgende Farben gewählt: 
-      <?php 
+      <?php
+    //formular abgesendet abfrage für echo keine farbe ausgewählt
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      $redSelected = false;
+      $whiteSelected = false;
+      $otherSelected = false;
+      $hasSelection = false;
+    // check farb
+    foreach ($_POST as $key => $value) {
+        if (str_contains($key, "color-")) {
+            $hasSelection = true;
+
+            if ($value == "rot") {
+                $redSelected = true;
+            } elseif ($value == "weiss") {
+                $whiteSelected = true;
+            } else {
+                $otherSelected = true;
+            }
+        }
+    }
+    //gibt liste aus
+    if ($hasSelection) {
+        echo "";
+        $selectedColors = [];
+
+        if ($redSelected) {
+            $selectedColors[] = "Rot";
+        }
+        if ($whiteSelected) {
+            $selectedColors[] = "Weiss";
+        }
+        if ($otherSelected) {               //andere da andere ja falsch sind da rot und weiss die gesuchten farben sind
+            $selectedColors[] = "<span style='color:red;'>(Und)falsche Farben du Idiot</span>";
+        }
+
+        echo implode(", ", $selectedColors); //implode funtkion fürs komma
+    } else {                                //deshalb oben die false abfragen für alle antworten / farben
+        echo "<span style='color:red;'>Keine Farben ausgewählt du Depp.</span>";
+    }
+}
       /*
         P04: Die ausgewählten Farbwerte herausfiltern und als einfache Liste 
         ausgeben. Es werden nur die Werte mit 'color-' im Schlüssel (Key) 
@@ -60,9 +101,28 @@
         P05: Zum gewählten Tier 'mammal' wird ein Feedback ausgegeben. Dazu verwenden wir
         switch() Verzweigungen.
       */
+      $selectedMammal = $_POST["mammal"];
+      switch ($selectedMammal) {
+        case "Rind":
+            echo "Mag ich auch, aber nur vom Grill.";
+            break;
+        case "Pferd":
+            echo "Ist lecker vom heißen Stein.";
+            break;
+        case "Ziege":
+            echo "Auch das kann man essen, aber ich habe lieber Gizi.";
+            break;
+        case "Mensch":
+            echo "Dümmstes Tier der Welt.";
+            break;
+        default:              //wenn depp nichts ausgewählt
+            echo "Zu dumm, um etwas auszuwählen?";
+    }
+    
       
 
       echo "<br><br>";
+
 
       /*
         P06: Im "comment"-Feld prüfen wir, ob gewisse Schimpfwörter wie
@@ -71,7 +131,15 @@
 
         Verwendete PHP Hilfsfunktionen: strlen(), strtolower(), str_replace()
       */
+      $comment = $_POST["comment"];
+      $swearWords = array("fuck", "arschloch", "wixxer");
       
+      foreach ($swearWords as $word) {        //str_replace ist dafür und str_ireplace für einfache erkennung
+          $censoredComment = str_ireplace($word, "#%$@", $comment);
+          $comment = $censoredComment;
+      }
+      
+      echo "Kommentar: " . $censoredComment;
       ?><br>
       
 
