92d537636c98a94cc530e3f369b80955d4151ce4
gabrielribery <opportunity.ruoss@gmail.com>
2023-07-06 09:37:09 +0200

    hin und her geschiebe mit dem ordner und header bearbeitet

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<footer class="bg-primary text-white text-center text-lg-start">
+    <div class="container p-4">
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+            © 2023 by Chris
+        </div>
+    </div>
+</footer>
--- index.php
+++ index.php
@@ -7,24 +7,9 @@
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css">
 </head>
 <body>
-<header class="bg-primary text-white text-center text-lg-start">
-    <div class="container p-4">
-        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
-            <?php 
-                /*
-                    Setze die variable 'people' and gib den
-                    Wert davon mit Hilfe von echo aus (String
-                    Template mit dem Ersten Wort 'Hello').
-                */ 
-
-
-                // Aufforderung
-
-            ?>
-        </div>
-    </div>
-</header>
-
+    <?php
+      include 'includes/header.php';
+    ?>
   <div class="container mt-4">
     <div class="row">
       <div class="col-md-4">
@@ -41,14 +26,9 @@
       </div>
     </div>
   </div>
-
-  <footer class="bg-primary text-white text-center text-lg-start">
-    <div class="container p-4">
-        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
-            © 2023 by Chris
-        </div>
-    </div>
-</footer>
+  <?php
+      include 'includes/footer.php';
+      ?>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.min.js"></script>
 </body>
 </html>
