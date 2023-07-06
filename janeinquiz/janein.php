03663c256ba26280aaf57780ae0b55aa15342b3d
gabrielribery <opportunity.ruoss@gmail.com>
2023-07-06 16:46:32 +0200

    hidden answer erstellt

--- janeinquiz/janein.php
+++ janeinquiz/janein.php
@@ -1,12 +1,14 @@
 <?php
 
-function ynquestion() {
+function ynquestion($qid, $questionTxt, $answers, $correctAnswerIndex) {
     echo "<p>&nbsp;</p>
     <p>$questionTxt</p>
     <input type='radio' id='r-$qid-0' name='radio-$qid' value='$answers[0]'>
     <label for='r-$qid-0'>$answers[0]</label><br>
     <input type='radio' id='r-$qid-1' name='radio-$qid' value='$answers[1]'>
     <label for='r-$qid-1'>$answers[1]</label><br><br>";
+
+    echo "<input type='hidden' name='correctAnswerIndex' value='$correctAnswerIndex'>"
 
 }
 
@@ -15,4 +17,8 @@ $questionTxt = "Bist du dumm?";
 $answers = array("ja", "nein");
 $correctAnswerIndex = 0;
 // $points = 1;
+
+ynquestion($qid, $questionTxt, $answers, $correctAnswerIndex)
 ?>
+
+
